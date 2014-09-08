f66037d07da65feefacdd8dd66cfccc50df5719b
Mitar <mitar.git@tnode.com>
2014-09-08 13:33:34 -0700

    Transitory package.

--- package.js
+++ package.js
@@ -1,20 +1,10 @@
 Package.describe({
   summary: "Meteor smart package for util node.js module",
   version: '0.2.3',
-  name: 'peerlibrary:util',
+  name: 'mrt:util',
   git: 'https://github.com/peerlibrary/meteor-util.git'
 });
 
 Package.on_use(function (api) {
-  api.versionsFrom('METEOR@0.9.1');
-  api.export('util');
-
-  api.add_files([
-    'server.js'
-  ], 'server');
-});
-
-Package.on_test(function (api) {
-  api.use(['peerlibrary:util', 'tinytest', 'test-helpers'], 'server');
-  api.add_files('tests.js', 'server');
+  api.imply('peerlibrary:util@0.2.3');
 });
--- server.js
+++
@@ -1,1 +0,0 @@
-util = Npm.require('util');
