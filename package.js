8793757a6ea2fc22456f1f6995d132d4874491fa
Mitar <mitar.git@tnode.com>
2016-08-03 23:35:59 -0700

    Version bump.

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'peerlibrary:util',
   summary: "Meteor smart package for util node.js module",
-  version: '0.4.0',
+  version: '0.5.0',
   git: 'https://github.com/peerlibrary/meteor-util.git'
 });
 
