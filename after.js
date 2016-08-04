37949436736635521a0201899801fb36ac3d043a
Mitar <mitar.git@tnode.com>
2016-08-03 23:34:42 -0700

    Style.

--- after.js
+++ after.js
@@ -3,4 +3,4 @@ util = exports;
 // util.print, util.puts, util.debug, util.error already throw an error through "deprecate".
 util._errnoException = util.debuglog = function () {
   throw new Error("Not implemented.");
-}
+};
